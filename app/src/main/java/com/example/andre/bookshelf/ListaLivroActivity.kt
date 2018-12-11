8195dd66ed9b579795f6ca86578ec841c18ef481
André Cantarin <a-cantarin@hotmail.com>
2018-12-11 21:19:41 -0200

    Criação do projeto. Primeiro commit

---
+++ app/src/main/java/com/example/andre/bookshelf/ListaLivroActivity.kt
@@ -0,0 +1,12 @@
+package com.example.andre.bookshelf
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ListaLivroActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lista_livro)
+    }
+}
